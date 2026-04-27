c1f0c0b442968fe86e015eab1ed28c7e66b6238f
Rameez Kamran <rameezkamran01@gmail.com>
2026-04-27 23:45:30 +0500

    Delete backend directory

--- backend/backend/db.py
+++
@@ -1,9 +0,0 @@
-import oracledb
-
-def get_connection():
-    conn = oracledb.connect(
-        user="parking_db",
-        password="pass123",
-        dsn="localhost/XEPDB1"
-    )
-    return conn
