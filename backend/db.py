0c3ffe630cb366d0883a839601797af69bd9f9e0
Rameez Kamran <rameezkamran01@gmail.com>
2026-05-06 08:10:42 +0500

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,10 @@
+import streamlit as st
 import oracledb
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_connection():
     conn = oracledb.connect(
-        user= os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        dsn=os.getenv("DB_DSN")
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        dsn=st.secrets["DB_DSN"]
     )
     return conn
